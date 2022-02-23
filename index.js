16010cef3d14e1ed6b46992b3b58bc6491424fb5
yani82 <yani.iman1@gmail.com>
2022-02-22 19:30:19 -0500

    instructions to abbreviate challenge

---
+++ index.js
@@ -0,0 +1,5 @@
+// Write a function called "abbreviate" that outputs a string.  
+// Given a string, keep the first and last letter and replace the letters in between by the length. 
+// e.g. internationalization => i18n, localization => l10n
+// 
+
